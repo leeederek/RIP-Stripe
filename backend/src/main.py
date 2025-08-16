9446a85ea33d63cc5f9874e6eaae5944db57fc9b
Jason Zhang <jz1242@gmail.com>
2025-08-16 18:24:00 -0400

    main change

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
 import fastapi
 from src.router import router
+from src.configs import genius_configs
+import json
+
+with open("genius_compliance_data.json", "r") as file:
+    genius_configs.DATA = json.load(file)
 
 server = fastapi.FastAPI()
 
-server.include_router(router, prefix="/api")
+server.include_router(router)
